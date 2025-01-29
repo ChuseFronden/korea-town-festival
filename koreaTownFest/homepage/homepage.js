1404f87f3c0f9a9f8b92d07b25bb64771b3ef7e2
ChuseFronden <49091038+ChuseFronden@users.noreply.github.com>
2025-01-29 03:48:39 +0200

    Add files via upload

--- koreaTownFest/homepage/homepage.js
+++ koreaTownFest/homepage/homepage.js
@@ -1,2 +1,9 @@
-// scripts.js
-console.log("Header Loaded");
+// homepage.js
+window.onload = function () {
+    fetch('homepage/homepage.html')  // Adjusted path to point to the 'homepage' folder
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('homepage-content').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading homepage content:', error));
+};
