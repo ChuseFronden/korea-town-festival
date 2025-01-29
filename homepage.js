139249931bcd74a1319fe2bdb4ee4d624404311d
ChuseFronden <49091038+ChuseFronden@users.noreply.github.com>
2025-01-29 04:23:40 +0200

    Delete homepage.js

--- homepage.js
+++
@@ -1,9 +0,0 @@
-// homepage.js
-window.onload = function () {
-    fetch('homepage/homepage.html')  // Adjusted path to point to the 'homepage' folder
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('homepage-content').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading homepage content:', error));
-};
